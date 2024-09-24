ea8e52fe0028ef258de3cddea40eb084897f626b
samibyrone <samsonibironke2@gmail.com>
2024-09-24 08:38:39 +0100

    generate password

---
+++ generate_password/random_password_generator.py
@@ -0,0 +1,20 @@
+import random
+import re
+from xmlrpc.client import boolean
+
+CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*()-_+=<>?"
+PASSWORD_LENGTH = 16
+
+def generate_password():
+    password = ''.join(random.choice(CHARACTERS) for _ in range(PASSWORD_LENGTH))
+    return password
+
+def is_strong_password(password):
+    regex = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&*()-_+=<>?]).{16,}$"
+    return boolean(re.match(regex, password))
+
+
+password = generate_password()
+
+print("\nGenerated Password Result :", password)
+print("\nIs This Strong Password Enough? :", is_strong_password(password))
---
+++ test/generate_password/test_random_password_generator.py
@@ -0,0 +1,40 @@
+import unittest
+
+from generate_password.random_password_generator import generate_password
+from generate_password.random_password_generator import is_strong_password
+
+
+class Generate_password(unittest.TestCase):
+
+    def test_for_password(self):
+        password = "SamsonIbironke"
+
+    def test_that_generate_password_length_to_be_16_in_length(self):
+        password = generate_password()
+        self.assertEqual(len(password), 16, "Password should not be more than 16 characters long")
+
+    def test_that_generate_password_that_contains_uppercase(self):
+        password = generate_password()
+        self.assertRegex(password, r".*[A-Z].*", "Password should contain at least minimum of one uppercase letter")
+
+    def test_that_generate_password_that_contains_lowercase(self):
+        password = generate_password()
+        self.assertRegex(password, r".*[a-z].*", "Password should contain at least minimum of one lowercase letter")
+
+    def test_that_password_is_strong_without_digit(self):
+        password = "Abcdefghijklmno@"
+        self.assertFalse(is_strong_password(password), "Password without digit should not be strong")
+
+    def test_that_generate_password_that_contains_digit(self):
+        password = generate_password()
+        self.assertRegex(password, r".*\d.*", "Password should contain at least minimum of one digit")
+
+    def test_password_with_more_than_required_length(self):
+        password = "Abcdefghijklmnop1@"
+        self.assertTrue(is_strong_password(password), "Password longer than required length and should be strong")
+
+    def test_password_with_lowercase(self):
+        password = "abcdefghijklmno1@"
+        self.assertFalse(is_strong_password(password), "Password without lowercase should not be strong")
+
+
